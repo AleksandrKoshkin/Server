200c3dd793069ecd104f53607f1ec5059606a97d
Aleksandr <aleksandr.koshkin.2000@mail.ru>
2023-04-27 19:22:41 +0300

    Two

--- src/Hosts.java
+++ src/Hosts.java
@@ -7,12 +7,15 @@ import java.net.Socket;
 
 public class Hosts {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8088);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        System.out.println("New connection accepted");
-        final String name = in.readLine();
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+        try (ServerSocket serverSocket = new ServerSocket(8088)) {
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("New connection accepted");
+            final String name = in.readLine();
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
